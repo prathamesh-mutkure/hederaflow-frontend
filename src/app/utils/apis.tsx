3e03719669546ce427dfabeef8c3d92eb10853fb
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-08-19 03:56:19 +0530

    Minor changes

--- src/app/utils/apis.tsx
+++ src/app/utils/apis.tsx
@@ -1,7 +1,7 @@
 // axiosInstance.ts
 import axios from "axios";
 
-const API_URL = "https://auto-stellar-backend.onrender.com";
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
